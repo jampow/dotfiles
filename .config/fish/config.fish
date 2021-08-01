197879ca1ba8ef7c9d333b07918f47f2dbe3f301
Gianpaulo M. Soares <jam_pow@hotmail.com>
2021-08-01 07:47:40 -0300

    added fish files

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias dfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+export TERM=screen-256color
+
