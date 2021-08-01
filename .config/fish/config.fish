bc8b1766384437b98758f31beace6a98af7eb045
Gianpaulo M. Soares <jam_pow@hotmail.com>
2021-08-01 08:14:09 -0300

    update dot files command to df

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias dfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias df='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 export TERM=screen-256color
 
