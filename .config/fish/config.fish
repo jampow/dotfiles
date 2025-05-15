fc9e902f05b004023be3729de282e7cad28c81e0
Gianpaulo M. Soares <jam_pow@hotmail.com>
2025-05-15 16:25:06 -0300

    enabled vi mode on fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# enables fish vi mode
+set -g fish_key_bindings fish_vi_key_bindings
+
 set fish_greeting
 
 alias df='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
@@ -14,11 +17,31 @@ set fish_greeting
 
 set -gx GPG_TTY (tty)
 
-source /opt/asdf-vm/asdf.fish
 fish_add_path $HOME/.local/bin
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 # pnpm
 set -gx PNPM_HOME "/home/gianpaulo/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 set -gx EDITOR "nvim"
+set -gx SUDO_EDITOR "lvim"
+
+# pnpm
+set -gx PNPM_HOME "/home/gianpaulo/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
 # pnpm end
